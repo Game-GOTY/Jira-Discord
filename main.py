09ddc60ad0736a07ad1b10355e2f52d4997c5a2d
Long Nguyen <knight1972001@gmail.com>
2025-03-27 23:45:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
 def jira_webhook():
     # print(f"Headers: {request.headers}")
     # print(f"Content-Type: {request.content_type}")
-    print(f"Raw Data: {request.get_data(as_text=True)}")
+    # print(f"Raw Data: {request.get_data(as_text=True)}")
     # print(f"Query Params: {request.args}")
 
     if not DISCORD_WEBHOOK_URL:
@@ -57,7 +57,7 @@ def jira_webhook():
             message = f"**{issue_key}** - **{event_type}**: **{issue_summary}** by **{user}** at {time}.\nURL: https://goty.atlassian.net/browse/{issue_key}/"
         else:
             message = f"**{issue_key}** - Status changed: **{status}** for **{issue_summary}** by **{user}** at {time}.\nURL: https://goty.atlassian.net/browse/{issue_key}/"
-        # response = requests.post(DISCORD_WEBHOOK_URL, json={"content": message})
+        response = requests.post(DISCORD_WEBHOOK_URL, json={"content": message})
         print(message)
         if response.status_code == 204:
             return "Success", 200
